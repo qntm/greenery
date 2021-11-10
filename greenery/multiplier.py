8d67ec644b2986f63fef1a9ec0711f6d7babdf4e
Robert Estelle <robertestelle@gmail.com>
2021-11-09 17:02:36 -0800

    Multiplier.__str__: avoid unnecessary look-before-leap indexing

--- greenery/multiplier.py
+++ greenery/multiplier.py
@@ -60,8 +60,12 @@ class Multiplier:
     def __str__(self, /) -> str:
         if self.max == Bound(0):
             raise Exception(f"Can't serialise a multiplier with bound {self.max!r}")
-        if self in symbolic.keys():
+
+        try:
             return symbolic[self]
+        except LookupError:
+            pass
+
         if self.min == self.max:
             return "{" + str(self.min) + "}"
         return "{" + str(self.min) + "," + str(self.max) + "}"
