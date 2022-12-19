4c4fc3aa93f9801cf6fc30fb780816172c568ff2
Robert Estelle <robertestelle@gmail.com>
2022-12-19 13:31:36 -0800

    export: greenery: explicitly define __all__

--- greenery/__init__.py
+++ greenery/__init__.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
-# In theory this should just pull in the `parse` function, but it turns out
-# that the act of performing any relative import suddenly makes EVERY MODULE
-# AVAILABLE IN SCOPE?
-# Ignore errors because we are in fact importing these values to re-export them
-from .bound import INF, Bound  # noqa: F401
-from .multiplier import PLUS, QM, STAR, Multiplier  # noqa: F401
-from .parse import parse  # noqa: F401
+__all__ = (
+    "Bound",
+    "INF",
+    "Multiplier",
+    "PLUS",
+    "QM",
+    "STAR",
+    "parse",
+)
 
-# WHY ARE THESE MODULE OBJECTS IN SCOPE SUDDENLY? I DIDN'T ASK FOR THEM
-# print(bound, fsm, charclass, rxelems)
-
-# This, however, is the *function*, *not* its containing module object?
-# print(parse)
+from .bound import INF, Bound
+from .multiplier import PLUS, QM, STAR, Multiplier
+from .parse import parse
