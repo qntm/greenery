b1a64670e9816e4144dc620556f707879afc390a
qntm <qntm@users.noreply.github.com>
2024-06-08 12:55:56 +0100

    pylint

--- greenery/charclass_test.py
+++ greenery/charclass_test.py
@@ -305,4 +305,4 @@ def test_charclass_by_category() -> None:
         else:
             out[cat].append(c)
     for cat, cs in out.items():
-        a = Charclass("".join(cs))
+        Charclass("".join(cs))
